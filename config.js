317b9d1efa6877824a910e6357e96aa191009e57
victor gichohi <victorgichohi48@gmail.com>
2015-09-27 23:11:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'https://rabidcode.herokuapp.com/',
+        url: 'https://rabidcode.herokuapp.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -23,7 +23,7 @@ config = {
 
         server: {
             host: '0.0.0.0',
-            port: ' process.env.PORT'
+            port: process.env.PORT
         }
     },
 
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'https://rabidcode.herokuapp.com/',
+        url: 'https://rabidcode.herokuapp.com',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
